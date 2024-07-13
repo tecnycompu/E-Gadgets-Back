d9d448444e110d97d5be89e3274661b0214910cf
tecnycompu <tecnycompu@gmail.com>
2024-07-13 08:50:11 -0500

    Update02

---
+++ controller/allUsers.js
@@ -0,0 +1,13 @@
+async function allUsers(){
+    try{
+
+    }catch(err){
+        res.status(400).json({
+            message : err.message || err,
+            error : true,
+            success : false  
+        })
+    }
+}
+
+module.exports = allUsers
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,15 @@ const userSignInController = require('../controller/userSignIn')
 const userDetailsController = require('../controller/userDetails')
 const authToken = require('../middleware/authToken')
 const userLogout = require('../controller/userLogout')
+const allUsers = require ('../controller/allUsers')
+
 
 router.post("/signup",userSingnUpcontroller)
 router.post("/signin",userSignInController)
 router.get("/user-details",authToken,userDetailsController)
 router.get("/userLogout", userLogout)
 
+// Admin panel
+router.get("/all-user",authToken,allUsers)
 module.exports = router
 
